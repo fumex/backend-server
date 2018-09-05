2061e4529de1a9b7c7e24c5758ef80403c652085
fumex <grego07h@gmail.com>
2018-09-05 13:05:10 -0500

    Seccion 12 terminada -backend

---
+++ README.md
@@ -0,0 +1,3 @@
+# BACKEN -SERVER
+Codigo del  backen serbe necesario para estableces el backend
+conectado con mongo usando Mongoose
--- config/config.js
+++ config/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED='este-es-un-seed-dificil';
+
+
+//Google
+module.exports.CLIENT_ID='111383521043-lb5j0v4d4aonfkfg5aeppfn6rslso6e5.apps.googleusercontent.com';
